347916c78f180ab4f6ca11c4d52cd06d9792245e
CeciLyu <cecilianlv@icloud.com>
2022-01-31 13:55:22 -0500

    t 24h

--- protein_script/run_pro_training.sh
+++ protein_script/run_pro_training.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=pro_gan_pro
 #SBATCH --account=def-mikeuoft # adjust this to match the accounting group you are using to submit jobs
-#SBATCH --time=00:10:00         # adjust this to match the walltime of your job
+#SBATCH --time=24:00:00         # adjust this to match the walltime of your job
 #SBATCH --nodes=1      
 #SBATCH --ntasks=1
 #SBATCH --gres=gpu:a100:1           # You need to request one GPU to be able to run AlphaFold properly
