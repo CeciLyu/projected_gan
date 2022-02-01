eb872acb4913e35407338b55cd5c96b33d84a6ed
CeciLyu <cecilianlv@icloud.com>
2022-01-31 19:22:04 -0500

    GRAHAM str pred AF reduced model1

---
+++ protein_script/alphafold_graham_reduced_multi.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#SBATCH --job-name=alphafold_GAN_gen_seqs
+#SBATCH --account=def-mikeuoft # adjust this to match the accounting group you are using to submit jobs
+#SBATCH --time=10:00:00         # adjust this to match the walltime of your job
+#SBATCH --nodes=1      
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:v100:1           # You need to request one GPU to be able to run AlphaFold properly
+#SBATCH --cpus-per-task=4      # adjust this if you are using parallel commands
+#SBATCH --mem=32G              # adjust this according to the memory requirement per node you need
+#SBATCH --mail-user=cecilianlv@icloud.com # adjust this to match your email address
+#SBATCH --mail-type=ALL
+#SBATCH --output=slurm-%j-alphafold_GAN_gen_seqs.out
+
+cd $SCRATCH 
+
+# Load your modules as before
+module load gcc openmpi cuda/11.1 cudacore/.11.1.1 cudnn/8.2.0 kalign hmmer hh-suite openmm python/3.7
+
+# Generate your virtual environment in $SLURM_TMPDIR
+virtualenv --no-download ${SLURM_TMPDIR}/my_env && source ${SLURM_TMPDIR}/my_env/bin/activate
+
+# Set the path to download dir
+DATA_DIR=$SCRATCH/ProteinGAN/generated_pro_seqs   # Set the appropriate path to your supporting data
+REPO_DIR=$SCRATCH/alphafold # Set the appropriate path to AlphaFold's cloned repo
+DOWNLOAD_DIR=$SCRATCH/alphafold/reduced_data  # Set the appropriate path to your downloaded data
+
+# Install alphafold and dependencies
+pip install --no-index six==1.15 numpy==1.19.2 scipy==1.4.1 pdbfixer alphafold
+
+for file in ${DATA_DIR}/*
+do
+	bash $SCRATCH/GFL/AF/script/alphafold_graham_reduced_single.sh $file
+done
+    
---
+++ protein_script/alphafold_graham_reduced_single.sh
@@ -0,0 +1,24 @@
+fasta_paths=$1
+
+echo ${fasta_paths}
+
+# Run your commands
+# ,model_2,model_3,model_4,model_5
+
+python ${REPO_DIR}/run_alphafold.py \
+   --data_dir ${DOWNLOAD_DIR} \
+   --uniref90_database_path ${DOWNLOAD_DIR}/uniref90/uniref90.fasta \
+   --mgnify_database_path ${DOWNLOAD_DIR}/mgnify/mgy_clusters_2018_12.fa \
+   --small_bfd_database_path ${DOWNLOAD_DIR}/small_bfd/bfd-first_non_consensus_sequences.fasta \
+   --pdb70_database_path ${DOWNLOAD_DIR}/pdb70/pdb70 \
+   --template_mmcif_dir ${DOWNLOAD_DIR}/pdb_mmcif/mmcif_files \
+   --obsolete_pdbs_path ${DOWNLOAD_DIR}/pdb_mmcif/obsolete.dat \
+   --fasta_paths ${fasta_paths} \
+   --max_template_date 2022-01-31 \
+   --hhblits_binary_path ${EBROOTHHMINSUITE}/bin/hhblits \
+   --hhsearch_binary_path ${EBROOTHHMINSUITE}/bin/hhsearch \
+   --jackhmmer_binary_path ${EBROOTHMMER}/bin/jackhmmer \
+   --kalign_binary_path ${EBROOTKALIGN}/bin/kalign \
+   --model_names model_1 \
+   --output_dir $SCRATCH/ProteinGAN/structure_gen_seq/ \
+   --preset reduced_dbs
