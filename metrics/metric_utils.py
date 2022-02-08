3fca8208ecd468552d9e662dde2c56dfbbcd6ea2
CeciLyu <cecilianlv@icloud.com>
2022-02-08 17:46:38 -0500

    typo

--- metrics/metric_utils.py
+++ metrics/metric_utils.py
@@ -260,7 +260,7 @@ def compute_feature_stats_for_dataset(opts, detector_url, detector_kwargs, rel_l
 
 #----------------------------------------------------------------------------
 
-def compute_feature_stats_for_generator(opts, detector_url, detector_kwargs, rel_lo=0, rel_hi=1, batch_size=64, batch_gen=None, swav=False, sfid=False, attn_res = None **stats_kwargs):
+def compute_feature_stats_for_generator(opts, detector_url, detector_kwargs, rel_lo=0, rel_hi=1, batch_size=64, batch_gen=None, swav=False, sfid=False, attn_res = None, **stats_kwargs):
     if batch_gen is None:
         batch_gen = min(batch_size, 4)
     assert batch_size % batch_gen == 0
