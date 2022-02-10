ac152951dbc1d4d51dd3a9a8d9a2e23481844755
CeciLyu <cecilianlv@icloud.com>
2022-02-10 15:59:38 -0500

    finding nan

--- training/loss.py
+++ training/loss.py
@@ -60,7 +60,9 @@ class ProjectedGANLoss(Loss):
             with torch.autograd.profiler.record_function('Gmain_forward'):
                 gen_img = self.run_G(gen_z, gen_c) # [B,C,H,W] [64,3,256,256]
                 norm_gen_img = (gen_img + 1)/ 255
-                sparse_loss_1 = torch.sum(torch.mul(-1*norm_gen_img, torch.log(norm_gen_img + 1e-12))) / gen_img.size(0)
+                print('torch.sum(torch.isnan(norm_gen_img))')
+                print(torch.sum(torch.isnan(norm_gen_img)))
+                sparse_loss_1 = torch.sum(torch.mul(-1*norm_gen_img, torch.log(norm_gen_img + 1e-12))) # gen_img.size(0)
                 print('torch.sum(torch.isnan(sparse_loss_1))')
                 print(torch.sum(torch.isnan(sparse_loss_1)))
                 sparse_loss_2 = torch.abs(torch.mean(gen_img)-1297*255/256/256)
