a59b6c92c7833e294e5dcb8c2048cef6e40e0732
CeciLyu <cecilianlv@icloud.com>
2022-02-10 16:22:53 -0500

    relu gen_img before norm

--- training/loss.py
+++ training/loss.py
@@ -61,7 +61,7 @@ class ProjectedGANLoss(Loss):
                 gen_img = self.run_G(gen_z, gen_c) # [B,C,H,W] [64,3,256,256]
                 print('gen_img.shape')
                 print(gen_img.shape)
-                norm_gen_img = (gen_img + 1)/ 255
+                norm_gen_img = torch.nn.functional.relu(gen_img)/ 255
                 print('print(torch.sum(norm_gen_img <= 0))')
                 print(torch.sum(norm_gen_img <= 0))
                 #sparse_loss_1 = torch.sum(torch.mul(-1*norm_gen_img, torch.log(norm_gen_img + 1e-12))) / gen_img.size(0)
@@ -71,8 +71,6 @@ class ProjectedGANLoss(Loss):
                 print('torch.sum(torch.isnan(sparse_loss_1))')
                 print(torch.sum(torch.isnan(sparse_loss_1)))
                 sparse_loss_2 = torch.abs(torch.mean(gen_img)-1297*255/256/256)
-                print('torch.sum(torch.isnan(sparse_loss_2))')
-                print(torch.sum(torch.isnan(sparse_loss_2)))
                 gen_logits = self.run_D(gen_img, gen_c, blur_sigma=blur_sigma)
                 loss_Gmain = (-gen_logits).mean() + sparse_loss_1 + sparse_loss_2
 
